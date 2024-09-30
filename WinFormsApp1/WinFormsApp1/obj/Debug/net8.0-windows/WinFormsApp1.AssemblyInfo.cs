c6ed9b83dc8de25c530346c9ecd3dfa7af251e4f
Mead Ahmed Fahim <fahimmeadahmed@gmail.com>
2024-09-30 15:28:03 +0600

    windows service project created

--- WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4976c9bddd5dcb566b3ecf8eee5406c595724ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e4aeb2afb69ea1c8343a6842569ed964180e2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WindowsServiceSample/Service1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace WindowsServiceSample
+{
+    public partial class Service1 : ServiceBase
+    {
+        private DateTime _startTime;
+        private string _logFilePath = @"D:\ServiceLogs\Service1Log.txt";
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _startTime = DateTime.Now;
+            string message = $"{_startTime}: Service has started.";
+            LogMessage(message);
+        }
+
+        protected override void OnStop()
+        {
+            DateTime stopTime = DateTime.Now;
+            TimeSpan runningTime = stopTime - _startTime;
+            string message = $"{stopTime}: Service has stopped. Service was running for {runningTime.TotalSeconds} seconds.";
+            LogMessage(message);
+        }
+
+        private void LogMessage(string message)
+        {
+            try
+            {
+                // Ensure the directory exists
+                Directory.CreateDirectory(Path.GetDirectoryName(_logFilePath));
+
+                // Grant read and write permissions if needed
+                SetFilePermissions(_logFilePath);
+
+                // Write the log message
+                using (StreamWriter writer = new StreamWriter(_logFilePath, true))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions appropriately, perhaps by logging to Event Viewer
+            }
+        }
+
+        private void SetFilePermissions(string filePath)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                FileSecurity fileSecurity = fileInfo.GetAccessControl();
+
+                // Grant read/write permissions to all users
+                fileSecurity.AddAccessRule(new FileSystemAccessRule(
+                    new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+                    FileSystemRights.Read | FileSystemRights.Write,
+                    AccessControlType.Allow));
+
+                fileInfo.SetAccessControl(fileSecurity);
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions appropriately, perhaps by logging to Event Viewer
+            }
+        }
+    }
+}
